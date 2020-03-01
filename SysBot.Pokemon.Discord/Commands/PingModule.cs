251e56cfb669985f969554c5f77068943c56f9ac
Lusamine <30205550+Lusamine@users.noreply.github.com>
2020-03-01 02:24:00 -0600

    The very best command.

--- SysBot.Pokemon.Discord/Commands/PingModule.cs
+++ SysBot.Pokemon.Discord/Commands/PingModule.cs
@@ -11,5 +11,13 @@ namespace SysBot.Pokemon.Discord
         {
             await ReplyAsync("Pong!").ConfigureAwait(false);
         }
+
+        [Command("hello"), Alias("hi")]
+        [Summary("Informs users that they are adorable.")]
+        [Priority(0)]
+        public async Task GreetUser()
+        {
+            await ReplyAsync("hi ur qt").ConfigureAwait(false);
+        }
     }
 }
