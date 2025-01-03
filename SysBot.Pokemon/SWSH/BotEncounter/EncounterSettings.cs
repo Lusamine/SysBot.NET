0e35f0a3ee3d865c4cc7152a30665371ec2bbb1e
Lusamine <30205550+Lusamine@users.noreply.github.com>
2025-01-02 18:23:39 -0600

    Move SWSH Curry Bot settings to its own folder

--- SysBot.Pokemon/SWSH/BotEncounter/EncounterSettings.cs
+++ SysBot.Pokemon/SWSH/BotEncounter/EncounterSettings.cs
@@ -21,26 +21,15 @@ public class EncounterSettings : IBotStateSettings, ICountSettings
     [TypeConverter(typeof(ExpandableObjectConverter))]
     public FossilSettings Fossil { get; set; } = new();
 
+    [Category(Settings)]
+    [TypeConverter(typeof(ExpandableObjectConverter))]
+    public CurrySettings Curry { get; set; } = new();
+
     [Category(Encounter), Description("The note for the Pokémon to select when starting a Dynamax Adventure. This can be 1-4.")]
     public int MaxLairNoteToPick { get; set; } = 1;
 
     [Category(Encounter), Description("The rental Pokémon to pick when a match is found. This can be 1-3.")]
     public int MaxLairRentalToPick { get; set; } = 1;
-
-    [Category(Encounter), Description("Sum of all encounter slots for this area.")]
-    public int CurrySlotTotal { get; set; } = 1;
-
-    [Category(Encounter), Description("The list of curry slots to target, formatted like \"20-33, 50-100\".")]
-    public string CurryTargetSlots { get; set; } = string.Empty;
-
-    [Category(Encounter), Description("Number of berries to add to each curry. Ranges from 1-10. Will use maximum if set to 0.")]
-    public int CurryBerriesToUse { get; set; }
-
-    [Category(Encounter), Description("Number of times to cook curry before rebooting the game to restore ingredients.")]
-    public int CurryTimesToCook { get; set; } = 30;
-
-    [Category(Encounter), Description("Chance of a curry spawn. Set this for the final curry grade using this routine. Koffing = 0.01, Wobbuffet = 0.05, Milcery = 0.15.")]
-    public float CurryTargetChance { get; set; } = 0.15f;
 
     [Category(Encounter), Description("When enabled, the bot will continue after finding a suitable match.")]
     public ContinueAfterMatch ContinueAfterMatch { get; set; } = ContinueAfterMatch.StopExit;
@@ -117,7 +106,7 @@ public class EncounterSettings : IBotStateSettings, ICountSettings
 
     public static void ReadTargetSlots(PokeTradeHubConfig hub, out IReadOnlyList<(int, int)> slots)
     {
-        slots = hub.EncounterSWSH.CurryTargetSlots.Split(',', StringSplitOptions.RemoveEmptyEntries)
+        slots = hub.EncounterSWSH.Curry.CurryTargetSlots.Split(',', StringSplitOptions.RemoveEmptyEntries)
             .Select(s => s.Split('-', StringSplitOptions.RemoveEmptyEntries))
             .Select(s => (int.Parse(s[0]), int.Parse(s[1])))
             .ToList();
