c0e3b12d59ffb4e8605b222f80b1c0f576bc3906
Lusamine <30205550+Lusamine@users.noreply.github.com>
2025-11-27 01:16:42 -0600

    Add A Spam Bot and RainSewers Simple Trick

--- SysBot.Pokemon/LZA/BotEncounter/EncounterBotBenchLZA.cs
+++ SysBot.Pokemon/LZA/BotEncounter/EncounterBotBenchLZA.cs
@@ -15,8 +15,8 @@ public sealed class EncounterBotBenchLZA(PokeBotState Config, PokeTradeHub<PA9> 
         while (!token.IsCancellationRequested)
         {
             await SetStick(LEFT, 0, -32768, 0_300, token).ConfigureAwait(false);
-            await ResetStick(token).ConfigureAwait(false);
             await Click(A, 0_100, token).ConfigureAwait(false);
+            await ResetStick(token).ConfigureAwait(false);
             while (!await IsOnOverworld(token).ConfigureAwait(false))
                 await Click(A, 0_200, token).ConfigureAwait(false);
             await Task.Delay(1_000, token).ConfigureAwait(false);
