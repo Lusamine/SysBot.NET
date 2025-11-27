cda4a9469039d689d7e09ef9028e204c665cc2ca
Lusamine <30205550+Lusamine@users.noreply.github.com>
2025-11-26 23:02:52 -0600

    Add basic LZA encounter bots
    - Reset bot for b1s2 for Floette
    - Bench macro
    - Example for simple tricks collection

---
+++ SysBot.Pokemon/LZA/BotEncounter/EncounterBotBenchLZA.cs
@@ -0,0 +1,25 @@
+using PKHeX.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Base.SwitchStick;
+
+namespace SysBot.Pokemon;
+
+public sealed class EncounterBotBenchLZA(PokeBotState Config, PokeTradeHub<PA9> Hub) : EncounterBotLZA(Config, Hub)
+{
+    protected override async Task EncounterLoop(SAV9ZA sav, CancellationToken token)
+    {
+        Log("Starting bench routine...");
+
+        while (!token.IsCancellationRequested)
+        {
+            await SetStick(LEFT, 0, -32768, 0_300, token).ConfigureAwait(false);
+            await ResetStick(token).ConfigureAwait(false);
+            await Click(A, 0_100, token).ConfigureAwait(false);
+            while (!await IsOnOverworld(token).ConfigureAwait(false))
+                await Click(A, 0_200, token).ConfigureAwait(false);
+            await Task.Delay(1_000, token).ConfigureAwait(false);
+        }
+    }
+}
