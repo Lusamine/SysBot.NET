889c7a6ce79b61ef392dbdd81fb18b5b22adc250
Lusamine <30205550+Lusamine@users.noreply.github.com>
2026-03-03 21:01:47 -0600

    Add FRLG encounter bots and RNG monitor

---
+++ SysBot.Pokemon/FRLG/BotEncounter/EncounterBotWildFRLG.cs
@@ -0,0 +1,101 @@
+using PKHeX.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Base.SwitchStick;
+using static SysBot.Pokemon.PokeDataOffsetsFRLG;
+
+namespace SysBot.Pokemon
+{
+    public sealed class EncounterBotWildFRLG(PokeBotState cfg, PokeTradeHub<PK3> hub) : EncounterBotFRLG(cfg, hub)
+    {
+        // Cached offsets that stay the same per session.
+        private uint EncounterOffset;
+        private bool Horizontal;
+
+        protected override async Task EncounterLoop(SAV3FRLG sav, CancellationToken token)
+        {
+            await InitializeSessionValues(sav).ConfigureAwait(false);
+
+            while (!token.IsCancellationRequested)
+            {
+                Log("Looking for a Pokémon...");
+                var tries = 0;
+
+                PK3? pknew;
+
+                do
+                {
+                    await WiggleInPlace(token).ConfigureAwait(false);
+                    pknew = await ReadUntilPresent(EncounterOffset, 0_050, 0_050, BoxFormatSlotSize, token).ConfigureAwait(false);
+                    if (++tries > 1000)
+                        break;
+                } while (pknew is null || pknew.Species == 0);
+
+                // Check for a match
+                if (pknew is not null && await HandleEncounter(pknew, token).ConfigureAwait(false))
+                    return;
+
+                Log("No match, running away...");
+                await EscapeBattle(token).ConfigureAwait(false);
+
+                Horizontal = !Horizontal;
+            }
+        }
+
+        // These don't change per session, and we access them frequently, so set these each time we start.
+        private async Task InitializeSessionValues(SAV3FRLG sav)
+        {
+            Log("Initializing session constants...");
+            EncounterOffset = LanguageVersionOffsetsFRLG.GetWildPokemonOffsetFromLanguageAndVersion((LanguageID)sav.Language, sav.Version);
+            Horizontal = true; // Always assume the user starts the bot facing up/down, and our first wiggles are left/right.
+        }
+
+        private async Task WiggleInPlace(CancellationToken token)
+        {
+            while (!await IsInBattle(token).ConfigureAwait(false))
+            {
+                if (Horizontal)
+                {
+                    for (var i = 0; i < 5; i++)
+                    {
+                        await SetStick(LEFT, -30000, 0, 0_050, token).ConfigureAwait(false);
+                        await SetStick(LEFT, 0, 0, 0_200, token).ConfigureAwait(false); // reset
+                        await SetStick(LEFT, 30000, 0, 0_050, token).ConfigureAwait(false);
+                        await SetStick(LEFT, 0, 0, 0_200, token).ConfigureAwait(false); // reset
+                    }
+                }
+                else
+                {
+                    for (var i = 0; i < 5; i++)
+                    {
+                        await SetStick(LEFT, 0, -30000, 0_050, token).ConfigureAwait(false);
+                        await SetStick(LEFT, 0, 0, 0_200, token).ConfigureAwait(false); // reset
+                        await SetStick(LEFT, 0, 30000, 0_050, token).ConfigureAwait(false);
+                        await SetStick(LEFT, 0, 0, 0_200, token).ConfigureAwait(false); // reset
+                    }
+                }
+            }
+        }
+
+        // Eventually replace this with a check for whether the battle menu has loaded.
+        private async Task EscapeBattle(CancellationToken token)
+        {
+            for (var i = 0; i < 12; i++)
+                await Click(B, 0_500, token).ConfigureAwait(false);
+
+            Log("Initiating run routine.");
+            while (await IsInBattle(token).ConfigureAwait(false))
+            {
+                await Click(DRIGHT, 0_200, token).ConfigureAwait(false);
+                await Click(DDOWN, 0_200, token).ConfigureAwait(false);
+                await Click(A, 0_500, token).ConfigureAwait(false);
+                await Click(B, 0_200, token).ConfigureAwait(false);
+                await Click(B, 0_200, token).ConfigureAwait(false);
+                await Click(B, 0_200, token).ConfigureAwait(false);
+                await Click(B, 0_200, token).ConfigureAwait(false);
+            }
+            await Task.Delay(0_500, token).ConfigureAwait(false);
+        }
+    }
+}
---
+++ SysBot.Pokemon/FRLG/BotEncounter/EncounterSettingsFRLG.cs
@@ -0,0 +1,81 @@
+using SysBot.Base;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading;
+
+namespace SysBot.Pokemon;
+
+public class EncounterSettingsFRLG : IBotStateSettings, ICountSettings
+{
+    private const string Counts = nameof(Counts);
+    private const string EncounterFRLG = nameof(EncounterFRLG);
+    public override string ToString() => "Encounter Bot FRLG Settings";
+
+    [Category(EncounterFRLG), Description("The method used by Reset bot to encounter Pokémon.")]
+    public EncounterModeFRLG EncounteringType { get; set; } = EncounterModeFRLG.StaticFRLG;
+
+    [Category(EncounterFRLG), Description("The Pokémon to purchase from the Game Corner.")]
+    public GameCornerPrizeFRLG GameCornerPrizeToPurchase { get; set; } = GameCornerPrizeFRLG.Abra;
+
+    [Category(EncounterFRLG), Description("The number of Pokémon to purchase from the Game Corner each reset.")]
+    public int GameCornerNumberToPurchase { get; set; } = 1;
+
+    [Category(EncounterFRLG), Description("Adds up to this many milliseconds before an encounter. Increases the number of potential Pokémon encountered due to FRLG's initial seeding.")]
+    public int RandomTimeBeforeEncounter { get; set; } = 1000;
+
+    [Category(EncounterFRLG), Description("Interval in milliseconds for the monitor to check the Main RNG state.")]
+    public int MonitorRefreshRate { get; set; }
+
+    [Category(EncounterFRLG), Description("Maximum total advances before the RNG monitor pauses the game by clicking X. Set to 0 to disable.")]
+    public int MaxTotalAdvances { get; set; }
+
+    [Category(EncounterFRLG), Description("When enabled, the bot will continue after finding a suitable match.")]
+    public ContinueAfterMatch ContinueAfterMatch { get; set; } = ContinueAfterMatch.StopExit;
+
+    [Category(EncounterFRLG), Description("When enabled, the screen will be turned off during normal bot loop operation to save power.")]
+    public bool ScreenOff { get; set; }
+
+    private int _completedWild;
+    private int _completedLegend;
+    private int _completedGift;
+
+    [Category(Counts), Description("Encountered Wild Pokémon")]
+    public int CompletedEncounters
+    {
+        get => _completedWild;
+        set => _completedWild = value;
+    }
+
+    [Category(Counts), Description("Encountered Legendary Pokémon")]
+    public int CompletedLegends
+    {
+        get => _completedLegend;
+        set => _completedLegend = value;
+    }
+
+    [Category(Counts), Description("Encountered Gift Pokémon")]
+    public int CompletedGifts
+    {
+        get => _completedGift;
+        set => _completedGift = value;
+    }
+
+    [Category(Counts), Description("When enabled, the counts will be emitted when a status check is requested.")]
+    public bool EmitCountsOnStatusCheck { get; set; }
+
+    public int AddCompletedEncounters() => Interlocked.Increment(ref _completedWild);
+    public int AddCompletedLegends() => Interlocked.Increment(ref _completedLegend);
+    public int AddCompletedGifts() => Interlocked.Increment(ref _completedGift);
+
+    public IEnumerable<string> GetNonZeroCounts()
+    {
+        if (!EmitCountsOnStatusCheck)
+            yield break;
+        if (CompletedEncounters != 0)
+            yield return $"Wild/Gift Encounters: {CompletedEncounters}";
+        if (CompletedLegends != 0)
+            yield return $"Legendary Encounters: {CompletedLegends}";
+        if (CompletedGifts != 0)
+            yield return $"Gifts Received: {CompletedGifts}";
+    }
+}
