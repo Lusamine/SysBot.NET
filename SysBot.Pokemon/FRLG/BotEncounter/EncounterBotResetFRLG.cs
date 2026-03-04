054c3950d62ce58de785bc8cf15daed003f83e3d
Lusamine <30205550+Lusamine@users.noreply.github.com>
2026-03-03 21:01:47 -0600

    Add FRLG encounter bots and RNG monitor

---
+++ SysBot.Pokemon/FRLG/BotEncounter/EncounterBotResetFRLG.cs
@@ -0,0 +1,79 @@
+using PKHeX.Core;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Pokemon.PokeDataOffsetsFRLG;
+
+namespace SysBot.Pokemon
+{
+    public sealed class EncounterBotResetFRLG(PokeBotState cfg, PokeTradeHub<PK3> hub) : EncounterBotFRLG(cfg, hub)
+    {
+        // Cached offsets that stay the same per session.
+        private uint EncounterOffset;
+        private int PartyCount;
+        private uint OverworldOffset;
+        private int ExtraTime;
+
+        protected override async Task EncounterLoop(SAV3FRLG sav, CancellationToken token)
+        {
+            await InitializeSessionValues(sav, token).ConfigureAwait(false);
+
+            while (!token.IsCancellationRequested)
+            {
+                Log("Looking for a Pokémon...");
+                var tries = 0;
+
+                PK3? pknew;
+
+                // Waits a random number of milliseconds to increase the number of possible RNG states.
+                if (ExtraTime > 0)
+                    await Task.Delay(Util.Rand.Next(0, ExtraTime), token).ConfigureAwait(false);
+
+                do
+                {
+                    await Click(A, 0_500, token).ConfigureAwait(false);
+                    pknew = await ReadUntilPresent(EncounterOffset, 0_050, 0_050, BoxFormatSlotSize, token).ConfigureAwait(false);
+                    if (++tries > 1000)
+                        break;
+                } while (pknew is null || pknew.Species == 0);
+
+                // Check for a match
+                if (pknew is not null && await HandleEncounter(pknew, token).ConfigureAwait(false))
+                    return;
+
+                Log("No match, resetting the game...");
+                await SoftResetGame(OverworldOffset, token).ConfigureAwait(false);
+            }
+        }
+
+        // These don't change per session, and we access them frequently, so set these each time we start.
+        private async Task InitializeSessionValues(SAV3FRLG sav, CancellationToken token)
+        {
+            Log("Initializing session constants...");
+            OverworldOffset = LanguageVersionOffsetsFRLG.GetOverworldOffsetFromLanguageAndVersion((LanguageID)sav.Language, sav.Version);
+
+            switch (Hub.Config.EncounterFRLG.EncounteringType)
+            {
+                case EncounterModeFRLG.StaticFRLG:
+                    EncounterOffset = LanguageVersionOffsetsFRLG.GetWildPokemonOffsetFromLanguageAndVersion((LanguageID)sav.Language, sav.Version);
+                    break;
+                case EncounterModeFRLG.StarterFRLG:
+                    PartyCount = 0;
+                    EncounterOffset = LanguageVersionOffsetsFRLG.GetPartyStartOffsetFromLanguageAndVersion((LanguageID)sav.Language, sav.Version);
+                    break;
+                case EncounterModeFRLG.GiftFRLG:
+                    PartyCount = await GetPartyCount(token).ConfigureAwait(false);
+                    if (PartyCount == 6)
+                        throw new System.Exception("All 6 party slots are full. Clear out at least one empty slot for gift encounters.");
+
+                    EncounterOffset = LanguageVersionOffsetsFRLG.GetPartyStartOffsetFromLanguageAndVersion((LanguageID)sav.Language, sav.Version);
+                    EncounterOffset += (uint)(PartyCount * 0x64); // The next empty slot.
+                    break;
+                default:
+                    throw new System.Exception("Encountering type is not handled. Select from Static, Starter, or Gift.");
+            }
+
+            ExtraTime = Hub.Config.EncounterFRLG.RandomTimeBeforeEncounter;
+        }
+    }
+}
---
+++ SysBot.Pokemon/FRLG/PokeRoutineExecutor9FRLG.cs
@@ -0,0 +1,219 @@
+using PKHeX.Core;
+using SysBot.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static SysBot.Base.SwitchButton;
+using static SysBot.Pokemon.PokeDataOffsetsFRLG;
+
+namespace SysBot.Pokemon;
+
+/// <summary>
+/// Executor for FR/LG games.
+/// </summary>
+public abstract class PokeRoutineExecutor3FRLG(PokeBotState Config) : PokeRoutineExecutor<PK3>(Config)
+{
+    protected PokeDataOffsetsFRLG Offsets { get; } = new();
+
+    public override Task<PK3> ReadPokemon(ulong offset, CancellationToken token) => ReadPokemon(offset, BoxFormatSlotSize, token);
+
+    public override async Task<PK3> ReadPokemon(ulong offset, int size, CancellationToken token)
+    {
+        var data = await Connection.ReadBytesAsync((uint)offset, size, token).ConfigureAwait(false);
+        return new PK3(data);
+    }
+
+    public async Task<PK3> ReadPokemonAbsolute(ulong offset, int size, CancellationToken token)
+    {
+        var data = await SwitchConnection.ReadBytesAbsoluteAsync(offset, size, token).ConfigureAwait(false);
+        return new PK3(data);
+    }
+
+    public override Task<PK3> ReadPokemonPointer(IEnumerable<long> jumps, int size, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override Task<PK3> ReadBoxPokemon(int box, int slot, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> ReadIsChanged(uint offset, byte[] original, CancellationToken token)
+    {
+        var result = await Connection.ReadBytesAsync(offset, original.Length, token).ConfigureAwait(false);
+        return !result.SequenceEqual(original);
+    }
+
+    public async Task<SAV3FRLG> IdentifyTrainer(CancellationToken token)
+    {
+        // Check if botbase is on the correct version or later.
+        await VerifyBotbaseVersion(token).ConfigureAwait(false);
+
+        // Check title so we can warn if mode is incorrect.
+        string title = await SwitchConnection.GetTitleID(token).ConfigureAwait(false);
+        if (!LanguageVersionOffsetsFRLG.IsTitleIDValidFRLG(title))
+            throw new Exception($"{title} is not a valid FRLG title. Is your mode correct?");
+
+        // Verify the game version.
+        var game_version = await SwitchConnection.GetGameInfo("version", token).ConfigureAwait(false);
+        if (!game_version.SequenceEqual(FRLGGameVersion))
+            throw new Exception($"Game version is not supported. Expected version {FRLGGameVersion}, and current game version is {game_version}.");
+
+        Log("Grabbing trainer data of host console...");
+        var sav = await GetFakeTrainerSAV(title, token).ConfigureAwait(false);
+        InitSaveData(sav);
+
+        if (!IsValidTrainerData())
+        {
+            await CheckForRAMShiftingApps(token).ConfigureAwait(false);
+            throw new Exception("Refer to the SysBot.NET wiki (https://github.com/kwsch/SysBot.NET/wiki/Troubleshooting) for more information.");
+        }
+
+        if ((TextSpeedOption)sav.TextSpeed < TextSpeedOption.Fast)
+            throw new Exception("Text speed should be set to FAST. Fix this for correct operation.");
+
+        return sav;
+    }
+
+    public async Task InitializeHardware(IBotStateSettings settings, CancellationToken token)
+    {
+        Log("Detaching on startup.");
+        await DetachController(token).ConfigureAwait(false);
+        if (settings.ScreenOff)
+        {
+            Log("Turning off screen.");
+            await SetScreen(ScreenState.Off, token).ConfigureAwait(false);
+        }
+    }
+
+    public async Task CleanExit(CancellationToken token)
+    {
+        await SetScreen(ScreenState.On, token).ConfigureAwait(false);
+        Log("Detaching controllers on routine exit.");
+        await DetachController(token).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Identifies the trainer information and loads the current runtime language.
+    /// </summary>
+    public async Task<SAV3FRLG> GetFakeTrainerSAV(string titleID, CancellationToken token)
+    {
+        var sav = new SAV3FRLG();
+        var block = sav.SmallBuffer;
+        var small_offset = await GetSmallOffset(titleID, token).ConfigureAwait(false);
+        if (small_offset == 0)
+            throw new Exception("Failed to get small block offset. Is your game version supported?");
+        var read = await Connection.ReadBytesAsync(small_offset, sav.Small.Length, token).ConfigureAwait(false);
+        read.CopyTo(block);
+        sav.Language = (int)LanguageVersionOffsetsFRLG.GetLanguageFromTitleID(titleID);
+        return sav;
+    }
+
+    public async Task<uint> GetSmallOffset(string titleID, CancellationToken token)
+    {
+        var current_seed_offset = LanguageVersionOffsetsFRLG.GetCurrentSeedOffsetFromTitleID(titleID);
+        if (current_seed_offset == 0)
+            return 0;
+        var small = current_seed_offset + SmallShift; // Pointer to small block.
+        var data = await Connection.ReadBytesAsync(small, 4, token).ConfigureAwait(false);
+        var small_offset = BitConverter.ToUInt32(data, 0); // This offset is relative to 0x02020000, which is where InitialSeed is.
+        if (small_offset == 0)
+            return 0; // In case it's not loaded.
+        return InitialSeed + (small_offset - 0x02020000); // Convert to absolute offset in the heap.        
+    }
+
+    public async Task<uint> GetLargeOffset(string titleID, CancellationToken token)
+    {
+        var current_seed_offset = LanguageVersionOffsetsFRLG.GetCurrentSeedOffsetFromTitleID(titleID);
+        if (current_seed_offset == 0)
+            return 0;
+        var large = current_seed_offset + LargeShift; // Pointer to large block.
+        var data = await Connection.ReadBytesAsync(large, 4, token).ConfigureAwait(false);
+        var large_offset = BitConverter.ToUInt32(data, 0); // This offset is relative to 0x02020000, which is where InitialSeed is.
+        if (large_offset == 0)
+            return 0; // In case it's not loaded.
+        return InitialSeed + (large_offset - 0x02020000); // Convert to absolute offset in the heap.
+    }
+
+    public async Task<uint> GetBoxStartOffset(string titleID, CancellationToken token)
+    {
+        var current_seed_offset = LanguageVersionOffsetsFRLG.GetCurrentSeedOffsetFromTitleID(titleID);
+        if (current_seed_offset == 0)
+            return 0;
+        var boxes = current_seed_offset + BoxStartShift; // Pointer to box data block.
+        var data = await Connection.ReadBytesAsync(boxes, 4, token).ConfigureAwait(false);
+        var box_start_offset = BitConverter.ToUInt32(data, 0); // This offset is relative to 0x02020000, which is where InitialSeed is.
+        if (box_start_offset == 0)
+            return 0; // In case it's not loaded.
+        return InitialSeed + (box_start_offset - 0x02020000); // Convert to absolute offset in the heap.
+    }
+
+    public async Task SoftResetGame(uint offset, CancellationToken token)
+    {
+        await PressAndHold([A, B, X, Y], 0_500, 0_500, token).ConfigureAwait(false);
+
+        while (!await IsOnOverworld(offset, token).ConfigureAwait(false))
+            await Click(A, 0_200, token).ConfigureAwait(false);
+        // The overworld check becomes 0xFF while showing the journal replays, so press B to speed it up.
+        for (int i = 0; i < 5; i++)
+            await Click(B, 0_200, token).ConfigureAwait(false);
+
+        Log("Back in the overworld!");
+    }
+
+    public async Task<bool> IsInBattle(CancellationToken token)
+    {
+        var data = await SwitchConnection.ReadBytesAsync(InBattleOffset, 1, token).ConfigureAwait(false);
+        return data[0] == 0x2;
+    }
+
+    public async Task<bool> IsOnOverworld(uint offset, CancellationToken token)
+    {
+        var data = await SwitchConnection.ReadBytesAsync(offset, 1, token).ConfigureAwait(false);
+        return data[0] == 0xFF;
+    }
+
+    // Used to check if the battle menu has loaded, so we can attempt to flee.
+    // This value starts at 1 and goes up each time a menu is opened.
+    public async Task<bool> IsOnBattleMenu(CancellationToken token)
+    {
+        var data = await Connection.ReadBytesAsync(BattleMenuOffset, 1, token).ConfigureAwait(false);
+        return data[0] >= 1;
+    }
+
+    public async Task<int> GetPartyCount(CancellationToken token)
+    {
+        var titleid = await SwitchConnection.GetTitleID(token).ConfigureAwait(false);
+        var offset = LanguageVersionOffsetsFRLG.GetPartyStartOffsetFromTitleID(titleid);
+
+        uint count = 0;
+        for (uint i = 0; i < 6; i++)
+        {
+            var data = await SwitchConnection.ReadBytesAsync(offset + (i * 0x64), 1, token).ConfigureAwait(false);
+            if (data[0] != 0)
+                count++;
+        }
+        return (int)count;
+    }
+
+    public async Task<ushort> GetInitialRNGState(bool log, CancellationToken token)
+    {
+        var data = await SwitchConnection.ReadBytesAsync(InitialSeed, 2, token).ConfigureAwait(false);
+        var seed = BitConverter.ToUInt16(data, 0);
+        if (log)
+            Log($"Initial RNG state: {seed:x4}");
+        return seed;
+    }
+
+    public async Task<uint> GetCurrentRNGState(uint offset, bool log, CancellationToken token)
+    {
+        var data = await SwitchConnection.ReadBytesAsync(offset, 4, token).ConfigureAwait(false);
+        var seed = BitConverter.ToUInt32(data, 0);
+        if (log)
+            Log($"RNG state: {seed:x8}");
+        return seed;
+    }
+}
